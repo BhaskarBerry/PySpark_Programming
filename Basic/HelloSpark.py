e78ef228adacdcf2fa13f88a3a0c2afe4f09d6d2
BhaskarBerry <bhaskarberry90@gmail.com>
2022-03-12 01:17:43 +0530

    Spark Application using Python

--- Basic/HelloSpark.py
+++ Basic/HelloSpark.py
@@ -3,6 +3,8 @@ from pyspark.sql import *
 from lib.logger import Log4j
 from lib.utils import *
 
+from Basic.loglib.utils import get_spark_app_config, load_survey_df, count_by_country
+
 if __name__ == "__main__":
     conf = get_spark_app_config()
 
@@ -20,10 +22,23 @@ if __name__ == "__main__":
 
     logger.info("Starting HelloSpark")
 
+    """
+    Actions: Read, write , collect, show, count
+    """
     survey_raw_df = load_survey_df(spark, sys.argv[1])
+    # survey_raw_df.show()
+
+    filtered_survey_df = survey_raw_df \
+        .where("Age < 40") \
+        .select("Age", "Gender", "Country", "state")
+
+    # filtered_survey_df.show()
+
     partitioned_survey_df = survey_raw_df.repartition(2)
     count_df = count_by_country(partitioned_survey_df)
-    count_df.show()
+    #count_df.show()
+    logger.info(count_df.collect())
 
     logger.info("Finished HelloSpark")
-    spark.stop()
+    input("Press Enter")
+    #spark.stop()
